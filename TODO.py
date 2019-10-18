bc2572d318c53807153e7dd9419cad1c5542bfcd
Artemiy Rodionov <wertins71@gmail.com>
2019-10-18 16:59:09 +0300

    Add TODO file

---
+++ TODO.py
@@ -0,0 +1,19 @@
+# comprehension
+# | ListComp(expr elt, comprehension* generators)
+# | SetComp(expr elt, comprehension* generators)
+# | DictComp(expr key, expr value, comprehension* generators)
+# | GeneratorExp(expr elt, comprehension* generators)
+[x for x in '']
+
+# lambda
+# | Lambda(arguments args, expr body)
+lambda x: x
+
+# with
+# | With(withitem* items, stmt* body, string? type_comment)
+# | AsyncWith(withitem* items, stmt* body, string? type_comment)
+with '' as f:
+    f
+async def x():
+    async with '' as z:
+        z
